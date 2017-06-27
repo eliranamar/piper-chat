af418ceb86e4ee360a841dee039b15ae05a1318f
eliranamar <eliranamar7@gmail.com>
2017-06-27 14:53:03 +0300

    backup

--- public/socket.js
+++ public/socket.js
@@ -1,12 +1,40 @@
 console.log('socket.js connected');
-var socket = io();
+
 
 var socket = io();
-$('form').submit(function () {
+
+// when client sends a message to server
+$('#messageForm').submit(function () {
   socket.emit('chat message', $('#m').val());
   $('#m').val('');
+  console.log('msg submitted');
   return false;
 });
-socket.on('chat message', function (msg) {
-  $('#messages').append($('<li>').text(msg));
+
+// when server sends a message to client
+socket.on('chat message', function (data) {
+  console.log(data.user);
+  $('#messages').append($('<li>').text('- ' + data.user + ': ' + data.text));
+  window.scrollTo(0, document.body.scrollHeigh);
+});
+
+// grab user login elements
+var $userForm = $('#userForm');
+var $nicknameInput = $('#nicknameInput');
+//on user login 
+$userForm.submit(function (e) {
+  e.preventDefault();
+
+  console.log('submit nickname');
+  console.log($nicknameInput.val());
+  if (!$nicknameInput.val()) {
+    alert('please enter nickname');
+    return false;
+  }
+  $(".i-am-centered").addClass("hide");
+  $(".container").removeClass("hide");
+  socket.emit('new user', $nicknameInput.val(), function () {
+
+  });
+  $nicknameInput.val('');
 });
--- server.js
+++ server.js
@@ -1,31 +1,62 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var users = [];
 var connections = [];
+var rooms = ['room1', 'room2'];
 var port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+//get req for chat rooms route
+app.get('/:id/chat', function () {
 
-
+});
+// when a user connects
 io.on('connection', function (socket) {
   console.log('======================');
   console.log('a user connected');
   console.log('======================');
+  connections.push(socket);
+  console.log('%s online connections', connections.length);
+
+  // when user login to chat room
+  socket.on('new user', function (data, callback) {
+    console.log(data);
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    console.log(users);
+  })
+
+  //when client send a message
   socket.on('chat message', function (msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    console.log(socket.username);
+    //send to every one
+    io.emit('chat message', {
+      text: msg,
+      user: socket.username
+    });
   });
+
+  // on disconnect of a user
   socket.on('disconnect', function () {
     console.log('======================');
     console.log('user disconnected');
     console.log('======================');
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('%s online connections', connections.length);
   });
 })
 
-server.listen(port, function () {
+server.listen(port, '0.0.0.0', function () {
   console.log('======================');
   console.log('listening on port 8000');
   console.log('======================');
