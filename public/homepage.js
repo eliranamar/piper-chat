1b91bd135193ff1a86a08c4810d229044c1a6cba
Rami Talisveiber <allindn@gmail.com>
2017-06-28 14:36:56 +0300

    'ramisFrontEnd'

---
+++ public/homepage.js
@@ -0,0 +1,28 @@
+console.log('homepage.js connected');
+
+var socket = io();
+var $nicknameInput = $('#nicknameInput');
+var $userForm = $('#userForm');
+
+
+$nicknameInput.focus();
+//making random room ids
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text + Math.floor(Math.random() * 99999);
+}
+
+$('#userForm').submit(function (e) {
+  e.preventDefault();
+  var roomId = makeid();
+  console.log('creating chat room');
+  socket.emit('create chat room', roomId, function () {
+
+ });
+  $nicknameInput.val('');
+  window.location = "http://localhost:8000/chat/"+roomId;
+
+});
